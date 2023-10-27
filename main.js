dc44f7551ae334d5620ae41a237456e38f4fa9a9
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-10-27 17:47:27 +0200

    impostato il button, manca l'event da settare

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ if(document.getElementById("minore").value){
     const priceyoung = kilometerP - ((20/100)*kilometerP);
     console.log(`Il prezzo del biglietto è ${priceyoung}`);
 }
+else if(document.getElementById("vecchio").value){
+        const priceold = kilometerP - ((40/100)*kilometerP);
+        console.log(`Il prezzo del biglietto è ${priceold}`);
+}
+
+let btn = document.getElementById("proceed");
+let input = document.getElementsByClassName("calc");
